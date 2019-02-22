497a430917d843564427fadc142874540a776e06
zhenglunsen <zhenglunsen@ioive.com>
2019-02-22 10:26:32 +0800

    linux

--- web/resources/views/Front/public.blade.php
+++ web/resources/views/Front/public.blade.php
@@ -42,8 +42,17 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 </script>
-    @yield('css')
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a97b8dfaf8b73f8cab734e8fc95ee176";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 </head>
+    @yield('css')
 <body>
 
 <div class="header">
